bca35fdd8157a242311b62a73ac62858a163065a
Ramiro <Ramiro@DESKTOP-PJKTA4N>
2019-06-04 23:12:39 -0500

    Validar usuario que sube

--- Desarrollo/src/main/java/com/desarrollo/test/Tester.java
+++ Desarrollo/src/main/java/com/desarrollo/test/Tester.java
@@ -3,7 +3,8 @@ package com.desarrollo.test;
 public class Tester {
 
 	public static void main(String[] args) {
-		System.out.println("********TEST_PRUEBAS*****");		
+		System.out.println("********TEST_PRUEBAS*****");
+		System.out.println("*************");
 	}
 
 }
