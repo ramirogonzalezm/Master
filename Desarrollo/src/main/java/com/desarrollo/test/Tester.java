dd109259d07191c3328656dc4cfc8f8ac7a1c7e9
Ramiro <Ramiro@DESKTOP-PJKTA4N>
2019-06-04 23:01:35 -0500

    Correccion del commit

--- Desarrollo/src/main/java/com/desarrollo/test/Tester.java
+++ Desarrollo/src/main/java/com/desarrollo/test/Tester.java
@@ -3,9 +3,7 @@ package com.desarrollo.test;
 public class Tester {
 
 	public static void main(String[] args) {
-		System.out.println("********TEST_PRUEBAS*****");
-		System.out.println("VALIDACION DE ENVIO POR MEDIO DE GIT");
-
+		System.out.println("********TEST_PRUEBAS*****");		
 	}
 
 }
