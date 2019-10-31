9d5cc216daaffbd12a9bc4434c8c2f9b386fd8c1
Dell <Dell@DESKTOP-O6DPPHT>
2019-10-31 12:07:47 -0600

    TEST DESARROLLO CON IGNORACIONES DE CODIGO.

--- Desarrollo/src/main/java/com/desarrollo/test/Tester.java
+++ Desarrollo/src/main/java/com/desarrollo/test/Tester.java
@@ -3,7 +3,7 @@ package com.desarrollo.test;
 public class Tester {
 
 	public static void main(String[] args) {
-		System.out.println("********TEST_PRUEBAS*****");
+		System.out.println("********TEST_PRUEBASRAMIROGONZALEZ*****");
 		System.out.println("*************");
 	}
 
