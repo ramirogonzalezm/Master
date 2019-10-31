6a1b706c52b39a8eeae0c87d02071149a1dcec39
Dell <Dell@DESKTOP-O6DPPHT>
2019-10-31 12:20:54 -0600

    CORRECTO ENVIO DE GIT.

--- Desarrollo/src/main/java/com/desarrollo/test/Tester.java
+++ Desarrollo/src/main/java/com/desarrollo/test/Tester.java
@@ -3,7 +3,8 @@ package com.desarrollo.test;
 public class Tester {
 
 	public static void main(String[] args) {
-		System.out.println("********TEST_PRUEBASRAMIROGONZALEZ*****");
+		System.out.println("********TEST_PRUEBASRAMIROGONZALEZMARTINEZ"
+				+ "*****");
 		System.out.println("*************");
 	}
 
