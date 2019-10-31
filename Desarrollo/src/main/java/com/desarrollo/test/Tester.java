380033b28640289e1e5abc376a789ced4648eeae
Dell <Dell@DESKTOP-O6DPPHT>
2019-10-31 11:55:56 -0600

    TEST PRUEBAS DELL 2.

--- Desarrollo/src/main/java/com/desarrollo/test/Tester.java
+++ Desarrollo/src/main/java/com/desarrollo/test/Tester.java
@@ -3,7 +3,7 @@ package com.desarrollo.test;
 public class Tester {
 
 	public static void main(String[] args) {
-		System.out.println("********TEST_PRUEBASRGM*****");
+		System.out.println("********TEST_PRUEBAS*****");
 		System.out.println("*************");
 	}
 
